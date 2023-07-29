7a75a864fd3394a0984b3b42f8ceeb46ca6a02ec
cruwmorgan <morganokeke@gmail.com>
2023-07-29 03:18:55 -0700

    pascal triangle - eturns a list of lists of integers representing the Pascal’s triangle of n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,27 @@
-# Recusrive method to create the series
-def computePascal(col, row):
-    # There are three things to compute
-    # 1. Left edge: col is 0
-    # 2. Right edge: col is same as row
-    if col == row or col == 0:
-        return 1
-    # 3. any other cell: col-1 + col of the previous row
-    else:
-        return computePascal(col - 1, row - 1) + computePascal(col, row - 1)
+#!/usr/bin/python3
 
+# Importing math module
+import math
 
-# Method to create the triangle for `N` row
 def pascal_triangle(n):
-    for r in range(n):
-        # upon observation, we can deduce the relation
-        # num_cols = num_rows + 1
-        for c in range(r + 1):
-            print(str(computePascal(c, r)), end=" ")
-        print("\n")
+    """ Representing the pascal's triangle of n. """
+    # Define matrix
+    matrix = []
+
+    # n stands for number of rows so we will iterate n times
+    for x in range(n):
+        # Define rows
+        rows = []
+
+        for y in range(x + 1):
+            # Find the combination of x and y
+            result = math.comb(x, y)
+
+            # Append result to inner list
+            rows.append(result)
+
+        # Append inner list to matrix
+        matrix.append(rows)
+
+    # Return list of list
+    return matrix
