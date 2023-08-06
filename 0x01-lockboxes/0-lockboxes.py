436206ec2dab3bfd6db3c06da71b3c013b0b8b53
cruwmorgan <morganokeke@gmail.com>
2023-08-06 04:38:46 -0700

    method that determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    method that determines if all the boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Return True if all boxes can be opened, else return False
+    """
+    unlocked_box = [0]
+    for i in unlocked_box:
+        for j in boxes[i]:
+            if j < len(boxes):
+                if j not in unlocked_box:
+                    unlocked_box.append(j)
+
+    # Look the the length of the open box and all the boxes
+    if len(boxes) == len(unlocked_box):
+        return True
+    else:
+        return False
