dba2c2c49ac1cd265509bad86cd7848cde6fa645
ctuwmorgan <morganokeke@gmail.com>
2023-09-15 07:41:10 +0000

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    """ Rotate a matrix 90 degrees clockwise.
+        Args:
+            matrix: given matrix to rotate
+        Return:
+            rotated matrix
+    """
+                
